5a31a65d2d13cbb513010d9215f01e30ec7617d6
Martin <gichurindungu@gmail.com>
2023-07-06 17:51:18 +0300

    add *

--- Models/base_model.py
+++ Models/base_model.py
@@ -2,7 +2,7 @@ import datetime
 import uuid
 class BaseModel:
     
-    def __init__(self):
+    def __init__(self,*args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
