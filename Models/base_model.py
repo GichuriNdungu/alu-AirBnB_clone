2a387fff8b13156795062065a19274098faeeec5
Martin <gichurindungu@gmail.com>
2023-07-06 17:39:44 +0300

    to_dict complete

--- Models/base_model.py
+++ Models/base_model.py
@@ -3,7 +3,7 @@ import uuid
 class BaseModel:
     
     def __init__(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
     
@@ -14,5 +14,12 @@ class BaseModel:
     def save(self):
         '''update updated_at with current time'''
         self.updated_at = datetime.datetime.now()
-    
-    
+    def to_dict(self):
+        '''serializes objects into Json'''
+        dict = {}
+        cls_name = self.__class__.__name__
+        self.__dict__['__class__'] = cls_name
+        self.__dict__['created_at'] = self.created_at.isoformat()
+        self.__dict__['updated_at'] = self.updated_at.isoformat()
+        dict = self.__dict__
+        return dict
