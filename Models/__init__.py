9747274da1cd61d918d1c52e0a60e7cc125b169c
Martin <gichurindungu@gmail.com>
2023-07-11 21:33:11 +0300

    Restart file_storage.py

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,4 +1,4 @@
-from models.engine.file_storage import FileStorage
+from Models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
--- Models/base_model.py
+++ Models/base_model.py
@@ -1,32 +1,30 @@
 import datetime
 import uuid
-import models
-
 class BaseModel:
     
     def __init__(self,*args, **kwargs):
         '''initialization of Basemodel objects'''
-        if len(kwargs) != 0:
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        if kwargs:
             if '__class__' in kwargs:
                 del kwargs['__class__']
-            for key, value in kwargs.items():
-                    if key in ['created_at', 'updated_at']:
-                       date_str = kwargs[key]
-                       value = datetime.datetime.fromisoformat(kwargs[key])
-                    setattr(self,key,value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-        models.storage.new(self)
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.datetime.strptime(kwargs['created_at'],tform)
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.datetime.strptime(kwargs['updated_at'], tform)
+            self.__dict__.update(kwargs)
+        Models.storage.new(self)
     def __str__(self):
         '''Modify __str__ '''
         class_name = self.__class__.__name__
-        return f"[{class_name}], ({self.id}), {self.__dict__}"
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
     def save(self):
         '''update updated_at with current time'''
         self.updated_at = datetime.datetime.now()
-        models.storage.save()
+        Models.storage.save()
     def to_dict(self):
         '''serializes objects into Json'''
         dict = self.__dict__.copy()
