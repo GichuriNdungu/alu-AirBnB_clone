910b6e44970c74cca9e904f3437e33faaa142a65
martin Ndungu <gichurindungu@gmail.com>
2023-07-16 21:10:28 +0300

    add show method

--- console.py
+++ console.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models import storage
 """A class that marks the entry point to the CLI"""
 
 class HBNBCommand(cmd.Cmd):
     prompt = 'HBNB: '
 
-    
+    classes  = {'BaseModel': BaseModel}
 
     def do_create(self, class_name):
-        classes  = {'BaseModel': BaseModel}
         '''creates an instance of a class
         saves the instance and prints id'''
         if len(class_name) == 0:
             print("** Class name missing")
-        elif class_name in classes.keys():
-            obj = classes[class_name]()
+        elif class_name in HBNBCommand.classes.keys():
+            obj = HBNBCommand.classes[class_name]()
             obj.save()
             print(obj.id)
         else:
             print('** Class not found')
+    def do_show(self, cls_id):
+
+        '''return str rep of obj
+        params; class_name, obj id'''
+        args = cls_id.split()
+        if len(args) == 0:
+            print("** Class name missing")
+        elif len(args) == 1:
+            print('** Instance id is missing')
+        else:
+            cls = args[0]
+            obj_id = args[1]
+            key = cls + '.' + obj_id
+            if cls not in HBNBCommand.classes.keys():
+                print("** Class doesn't exist")
+            else:
+                all_objs = storage.all()
+                obj = all_objs[key]
+                print(obj)
     def do_quit(self, arg):
         '''exits the program'''
         print('''Exiting...''')
