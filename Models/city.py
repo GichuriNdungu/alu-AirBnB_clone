a23b554cbc835f05b726dad652a3fc431296ac0b
martin Ndungu <gichurindungu@gmail.com>
2023-07-17 13:18:06 +0300

    add new classes

---
+++ Models/city.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+'''user class that inherits from BaseModel'''
+
+class City (BaseModel):
+    name = ''
+    state_id = ''
---
+++ Models/place.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+'''user class that inherits from BaseModel'''
+
+class Place (BaseModel):
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
